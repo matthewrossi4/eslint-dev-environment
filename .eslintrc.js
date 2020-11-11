0cfd79a412f581621af60bb352e622d0e24578c8
Matthew Rossi <mrossi4@uncc.edu>
2020-11-10 23:56:08 -0500

    Updated eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,15 @@
 module.exports = {
-  extends: 'standard'
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  rules: {
+  }
 }
